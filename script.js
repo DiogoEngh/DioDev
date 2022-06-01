528cab914c234e977ccdb4b3f907dd5a9cdb69a2
DiogoEngh <97733618+DiogoEngh@users.noreply.github.com>
2022-06-01 16:27:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const button_menu = document.getElementById("button_menu")
+const button_night = document.getElementById("button_night")
 const menu_lateral = document.getElementById("menu_lateral")
+const headerInicio = document.getElementById("header_inicio")
 var menu_active = false
+var nightActive = false
 const line_1 = document.getElementById("line_1")
 const line_2 = document.getElementById("line_2")
 const line_3 = document.getElementById("line_3")
-button_menu.addEventListener("click",()=>{
+
+var menuSide = () => {
     switch(menu_active){
       case false:
         menu_active = true
@@ -25,4 +29,22 @@ button_menu.addEventListener("click",()=>{
         line_2.style.backgroundColor = "white"
         break
     }
-})
+}
+
+var nightMode = () => {
+  switch(nightActive){
+    case false:
+      nightActive = true
+      headerInicio.style.backgroundColor = "#253166"
+      menu_lateral.style.backgroundColor = "#253166"
+      break
+    default:
+      nightActive = false
+      headerInicio.style.backgroundColor = "dodgerblue"
+      menu_lateral.style.backgroundColor = "dodgerblue"
+      break
+  }
+}
+
+button_menu.addEventListener("click",menuSide)
+button_night.addEventListener("click",nightMode)
