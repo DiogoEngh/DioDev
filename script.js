3dc4bb0f0138271f35ec721e615afec746efb4c4
DiogoEngh <97733618+DiogoEngh@users.noreply.github.com>
2022-06-14 17:32:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const button_menu = document.getElementById("button_menu")
-const button_night = document.getElementById("button_night")
 const menu_lateral = document.getElementById("menu_lateral")
 const headerInicio = document.getElementById("header_inicio")
+const mainInicio = document.getElementById("main_inicio")
+const footerInicio = document.getElementById("footer_inicio")
 var menu_active = false
-var nightActive = false
 const line_1 = document.getElementById("line_1")
 const line_2 = document.getElementById("line_2")
 const line_3 = document.getElementById("line_3")
@@ -13,9 +13,9 @@ var menuSide = () => {
       case false:
         menu_active = true
         menu_lateral.style.width = "100%"
-        line_1.style.top = "1.1rem"
+        line_1.style.top = "1.15rem"
         line_1.style.transform = "rotate(45deg)"
-        line_3.style.bottom = "1.1rem"
+        line_3.style.bottom = "1.15rem"
         line_3.style.transform = "rotate(-45deg)"
         line_2.style.backgroundColor = "transparent"
         break
@@ -31,20 +31,4 @@ var menuSide = () => {
     }
 }
 
-var nightMode = () => {
-  switch(nightActive){
-    case false:
-      nightActive = true
-      headerInicio.style.backgroundColor = "#253166"
-      menu_lateral.style.backgroundColor = "#253166"
-      break
-    default:
-      nightActive = false
-      headerInicio.style.backgroundColor = "dodgerblue"
-      menu_lateral.style.backgroundColor = "dodgerblue"
-      break
-  }
-}
-
 button_menu.addEventListener("click",menuSide)
-button_night.addEventListener("click",nightMode)
