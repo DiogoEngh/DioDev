38560776c7fe6684b8fda2455c959756061789f6
DiogoEngh <97733618+DiogoEngh@users.noreply.github.com>
2022-06-24 20:31:26 -0300

    Update and rename script.js to buttonMenu.js

---
+++ buttonMenu.js
@@ -0,0 +1,31 @@
+const button = document.getElementById("menu")
+const lineTop = document.getElementById("line1")
+const lineMidlle = document.getElementById("line2")
+const lineBottom = document.getElementById("line3")
+const inicioAside = document.getElementById("inicioAside")
+let interrupt = true
+
+
+button.addEventListener('click',()=>{
+	if(interrupt){
+		lineTop.style.transform = "rotate(-45deg)"
+		lineBottom.style.transform = "rotate(45deg)"
+		lineTop.style.top = ".9rem"
+		lineBottom.style.top = ".9rem"
+		lineTop.style.width = "2rem"
+		lineBottom.style.width = "2rem"
+		lineMidlle.style.backgroundColor = "transparent"
+		inicioAside.style.width = "20rem"
+		interrupt = false
+	}else{
+		lineTop.style.transform = "rotate(0deg)"
+		lineBottom.style.transform = "rotate(0deg)"
+		lineTop.style.top = ".1rem"
+		lineBottom.style.top = "1.7rem"
+		lineTop.style.width = "1.3rem"
+		lineBottom.style.width = "1.3rem"
+		lineMidlle.style.backgroundColor = "white"
+		inicioAside.style.width = "0rem"
+		interrupt = true
+	}
+})
